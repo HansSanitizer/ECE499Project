18af4593c3b9c9d300b89319f50c9ada16ef3c75
Jared M <jaredgrm@gmail.com>
2018-06-23 22:31:37 -0700

    End of day commit
    Trying bulk contour data manipulation

---
+++ handBombing2.py
@@ -0,0 +1,118 @@
+from matplotlib import pyplot as plt
+import cv2 as cv
+print(cv.__version__)
+import numpy as np
+bw = cv.imread('Images/ymmly1.jpg', cv.IMREAD_GRAYSCALE)
+#plt.imshow(bw, cmap='gray'), plt.show()
+
+# IF THIS NEXT LINE CAUSES TROUBLE, JUST REMOVE IT, IT IS SUPPOSED TO SPEED THINGS UP USING OPENCL
+# REFLINK: https://www.learnopencv.com/opencv-transparent-api/
+bwUmat = cv.UMat(bw)
+height, width = bw.shape
+print("The original image height is ", height)
+print("The original image width is ", width)
+black = np.zeros(bw.shape, np.uint8)
+# bwCircle = bw.copy()
+# bwCircleUmat = cv.UMat(bwCircle)
+# bwCircleBlurred = cv.UMat.get(cv.medianBlur(bwCircleUmat, 15))
+# # plt.imshow(bwCircle),plt.show()
+# print("Blurred image for circle finding")
+# rows = bwCircle.shape[0]
+#
+# # Find the small circle (minima of region of interest, also better for center of record determination)
+# circles = cv.HoughCircles(bwCircleBlurred, cv.HOUGH_GRADIENT,
+#                           1, rows/8, param1=90, param2=50,
+#                           minRadius=int(1600), maxRadius=int(1800))
+# if circles is None:
+#     print("Found 0 Circles")
+# else:
+#     circles = np.uint16(np.around(circles))
+#     for i in circles[0, :]:
+#         centerSmall = (i[0], i[1])
+#         #
+#         radiusSmall = i[2] + 800
+#         # Draw the center of the circle
+#         cv.circle(bw, centerSmall, 1, (0, 100, 100), 50)
+#         # Draw the roi on a black image
+#         cv.circle(black, centerSmall, (radiusSmall + 1000), (255,255,255), 20)
+#         cv.circle(black, centerSmall, (radiusSmall + 1100), (255,255,255), 20)
+#         # Draw the circle found on the original image
+#         cv.circle(bw, centerSmall, radiusSmall, (255, 0, 255), 50)
+#         print("Detected Small Circle", i)
+
+# Pre-determined magic numbers, see above for attempted method
+centerSmall = (6790, 8775)
+radiusSmall = 10000 - 6790
+
+
+#plt.imshow(bw, cmap='gray'), plt.show()
+#plt.imshow(black, cmap='gray'), plt.show()
+print("Record Center(x, y) is: ", centerSmall)
+black2 = np.zeros(bw.shape, np.uint8)
+circOut = cv.circle(black2, centerSmall, (radiusSmall + 3200), (255, 255, 255), -1)
+circIn = cv.circle(black2, centerSmall, (radiusSmall + 3180), (0, 0, 0), -1)
+fin = cv.bitwise_and(bw, black2)
+plt.imshow(fin), plt.show()
+
+# Apply Threshold and detect contours
+thresh_adapt = cv.adaptiveThreshold(fin, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 45, 0)
+im2, contours, hierarchy = cv.findContours(thresh_adapt, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+print("Detected number of contours:")
+print(len(contours))
+
+# # Find largest contour
+# largestArea = 0
+# for i in range(0, len(contours)):
+#     a = cv.contourArea(contours[i])
+#     if a > largestArea:
+#         # print("Found a larger contour:")
+#         print(a)
+#         largestArea = a
+#         largest_contour_index = i
+#         bounding_rect = cv.boundingRect(contours[i])
+#     # else:
+#         # print("didn't work")
+# print("Found largest contour:")
+# print(largestArea)
+# print("Largest contour was at index:")
+# print(i)
+cv.drawContours(fin, contours, -1, (255,255,255), 1)
+plt.imshow(fin), plt.show()
+# print("trying to extract some useful values:")
+# testContours = contours[i]
+# print(contours[i][0,0,0])
+# print(contours[i][0,0,1])
+
+# Try to convert all contour data to r, theta data
+
+# Make a huge xy data set of every xy contour point
+x = centerSmall[0] + contours[0][0, 0, 0]
+y = centerSmall[1] + contours[0][0, 0, 1]
+rho = np.sqrt(x**2 + y**2)
+theta = np.arctan(y/x)
+
+contoursThetaRho = np.array([theta, rho])
+print("Printing Contours XY:")
+print(contoursThetaRho)
+for i in range(1, len(contours)):
+    for j in range(0, len(contours[i])):
+        x = centerSmall[0] + contours[i][j, 0, 0]
+        y = centerSmall[1] + contours[i][j, 0, 1]
+        rho = np.sqrt(x**2 + y**2)
+        theta = (np.arctan2(y, x))
+        b = np.array([theta, rho])
+        # keep adding new rows
+        contoursThetaRho = np.column_stack((contoursThetaRho.T, b)).T
+
+print("Contour XY data list")
+print(contoursThetaRho)
+plt.plot(contoursThetaRho), plt.show()
+
+
+# Try canny edge detection as a method instead
+# thresh_adapt = cv.adaptiveThreshold(bw, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 45, 0)
+# im2, contours, hierarchy = cv.findContours(thresh_adapt, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# print(len(contours))
+# black = np.zeros(bw.shape, np.uint8)
+# cv.drawContours(black, contours, -1, (255, 255, 255), 50)
+# plt.imshow(bw), plt.show()
