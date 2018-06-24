ce5090def95b900d077241c1218fd49e50d8037e
Nolan Meske <nolanmeske@icloud.com>
2018-06-24 13:12:38 -0700

    Added AudioPlayer
    It plays a wave file to completion. No bells or whistles.

---
+++ AudioPlayer_TestScript.py
@@ -0,0 +1,12 @@
+""" This Script Tests the functionality of the AudioPlayer module.
+
+    Warning: there is a very real danger of screaming feedback, so
+             lower speaker volume before using!
+"""
+import AudioPlayer
+
+player = AudioPlayer.AudioPlayer()
+
+player.load("/Users/nolanmeske/Documents/Music Projects/4280/Orbital/renders/Orbital.wav")
+player.play()
+del player
